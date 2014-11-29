9cabb3348959fa64bc34d8cd380618546efca2fa
applenick <applenick3@me.com>
2014-11-29 10:30:52 -0800

    Init Project

---
+++ src/main/java/com/applenick/theforce/TheForce.java
@@ -0,0 +1,10 @@
+package com.applenick.theforce;
+
+/************************************************
+			 Created By AppleNick
+Copyright © 2014 , AppleNick, All rights reserved.
+			http://applenick.com
+ *************************************************/
+public class TheForce {
+
+}
